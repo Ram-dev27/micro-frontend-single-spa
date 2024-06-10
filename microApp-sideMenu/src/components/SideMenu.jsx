0c3626867ae656257547efe8f7572b669c9fce39
ram <ramachandran.p@optisolbusiness.com>
2024-06-10 10:50:43 +0530

    fix:location nav bar fix

--- microApp-sideMenu/src/components/SideMenu.jsx
+++ microApp-sideMenu/src/components/SideMenu.jsx
@@ -6,14 +6,19 @@ const SideMenu = () => {
   const location = useLocation();
 
   const getLinkClass = (path) => {
-    return location.pathname === path 
+    if (path === '/') {
+      return location.pathname === path 
+        ? "text-black no-underline block p-2 ps-4 bg-gray-300" 
+        : "text-black no-underline block p-2 ps-4 hover:bg-gray-200";
+    }
+    return location.pathname.startsWith(path) 
       ? "text-black no-underline block p-2 ps-4 bg-gray-300" 
       : "text-black no-underline block p-2 ps-4 hover:bg-gray-200";
   };
 
   return (
     <div className="w-[200px] h-full fixed bg-gray-100 box-border">
-      <h4 className=' text-center pt-2 fs-4 fw-bold'>Micro-frontend</h4>
+      <h4 className='text-center pt-2 fs-4 fw-bold'>Micro-frontend</h4>
       <ul className="list-none p-0">
         <li>
           <a href="/" onClick={navigateToUrl} className={getLinkClass('/')}>Home</a>
